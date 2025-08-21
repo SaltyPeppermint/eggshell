dd2fb3a8d0a2ae1663cd11a900a013c43a698250
Nicole Heinimann <heinimann@tu-berlin.de>
2025-08-21 11:04:48 +0200

    Rolled back the crossbeam queue since it can lead to unfortunate complexity with no deduplicaiton.

--- src/analysis/commutative_semigroup/mod.rs
+++ src/analysis/commutative_semigroup/mod.rs
@@ -4,10 +4,9 @@ mod loop_free_count;
 
 use std::fmt::Debug;
 use std::iter::{Product, Sum};
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 
-use crossbeam::queue::SegQueue;
 use dashmap::DashMap;
 use egg::{Analysis, DidMerge, EGraph, Id, Language};
 use hashbrown::HashMap;
@@ -47,7 +46,7 @@ where
             egraph: &EGraph<L, N>,
             analysis: &B,
             data: &Arc<DashMap<Id, B::Data>>,
-            queue: &SegQueue<Id>,
+            analysis_pending: &Arc<Mutex<UniqueQueue<Id>>>,
         ) where
             L: Language + Sync + Send,
             L::Discriminant: Sync,
@@ -58,7 +57,8 @@ where
         {
             // Potentially, this might lead to a situation where only one thread is working on the queue.
             // This has not been observed in practice, but it is a potential bottleneck.
-            while let Some(id) = queue.pop() {
+            while let Some(id) = { analysis_pending.lock().unwrap().pop() } {
+                // Drop lock at the end of the scope
                 let canonical_id = egraph.find(id);
                 debug_assert_eq!(canonical_id, id);
                 let eclass = &egraph[canonical_id];
@@ -86,14 +86,15 @@ where
                         .get(&eclass.id)
                         .is_some_and(|v| v.value() == &computed_data))
                     {
-                        for i in eclass.parents().map(|p| egraph.find(p)) {
-                            queue.push(i);
-                        }
+                        analysis_pending
+                            .lock()
+                            .unwrap()
+                            .extend(eclass.parents().map(|p| egraph.find(p)));
                         data.insert(eclass.id, computed_data);
                     }
                 } else {
                     assert!(!eclass.nodes.is_empty());
-                    queue.push(canonical_id);
+                    analysis_pending.lock().unwrap().insert(canonical_id);
                 }
             }
         }
@@ -102,25 +103,24 @@ where
 
         // We start at the leaves, since they have no children and can be directly evaluated.
 
-        let queue = Arc::new(SegQueue::new());
-
-        for id in egraph
+        let analysis_pending = egraph
             .classes()
             .filter(|eclass| eclass.nodes.iter().any(|enode| enode.is_leaf()))
+            // No egraph.find since we are taking the id directly from the eclass
             .map(|eclass| eclass.id)
-        {
-            queue.push(id);
-        }
+            .collect();
+
+        let par_analysis_pending = Arc::new(Mutex::new(analysis_pending));
 
         let par_data = Arc::new(DashMap::new());
         let pd = thread::scope(move |scope| {
             for i in 0..thread::available_parallelism().unwrap().get() {
                 let thread_data = par_data.clone();
-                let thread_queue = queue.clone();
+                let thread_analysis_pending = par_analysis_pending.clone();
 
                 scope.spawn(move || {
                     debug!("Thread #{i} started!");
-                    resolve_pending_analysis(egraph, self, &thread_data, &thread_queue);
+                    resolve_pending_analysis(egraph, self, &thread_data, &thread_analysis_pending);
                     debug!("Thread #{i} finished!");
                 });
             }
