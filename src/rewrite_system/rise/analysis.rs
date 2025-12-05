f33e75fcd21d5eee2c1da022c110e6eeca189d30
Nicole Heinimann <heinimann@tu-berlin.de>
2025-12-05 20:35:35 +0100

    added analysis that should always insert the smallest nat expr

--- src/rewrite_system/rise/analysis.rs
+++ src/rewrite_system/rise/analysis.rs
@@ -1,22 +1,38 @@
-use egg::{Analysis, DidMerge, EGraph, Language, RecExpr};
+use egg::{Analysis, DidMerge, EGraph, Id, Language, RecExpr};
 use hashbrown::HashSet;
 
 use crate::rewrite_system::rise::kind::Kindable;
+use crate::rewrite_system::rise::nat::try_simplify;
 
-use super::nat::Math;
 use super::{Index, Rise};
 
 #[derive(Default, Debug)]
-pub struct RiseAnalysis(HashSet<(RecExpr<Math>, RecExpr<Math>)>);
+pub struct RiseAnalysis {
+    nat_eq_cache: EGraph<Rise, ()>,
+}
 
 impl RiseAnalysis {
     #[must_use]
     pub fn new() -> Self {
-        Self(HashSet::default())
+        Self {
+            nat_eq_cache: EGraph::default(),
+        }
     }
 
-    pub fn get_mut_term_bank(&mut self) -> &mut HashSet<(RecExpr<Math>, RecExpr<Math>)> {
-        &mut self.0
+    pub fn check_cache_equiv(&mut self, lhs: &RecExpr<Rise>, rhs: &RecExpr<Rise>) -> Option<bool> {
+        if let Some(lhs_id) = self.nat_eq_cache.lookup_expr(lhs)
+            && let Some(rhs_id) = self.nat_eq_cache.lookup_expr(rhs)
+        {
+            Some(lhs_id == rhs_id)
+        } else {
+            None
+        }
+    }
+
+    pub fn add_pair_to_cache(&mut self, lhs: &RecExpr<Rise>, rhs: &RecExpr<Rise>) {
+        let lhs_id = self.nat_eq_cache.add_expr(lhs);
+        let rhs_id = self.nat_eq_cache.add_expr(rhs);
+        self.nat_eq_cache.union(lhs_id, rhs_id);
     }
 }
 
@@ -24,6 +40,7 @@ impl RiseAnalysis {
 pub struct AnalysisData {
     pub free: HashSet<Index>,
     pub beta_extract: RecExpr<Rise>,
+    pub simple_nat: Option<RecExpr<Rise>>,
 }
 
 impl Analysis<Rise> for RiseAnalysis {
@@ -32,15 +49,28 @@ impl Analysis<Rise> for RiseAnalysis {
     fn merge(&mut self, to: &mut AnalysisData, from: AnalysisData) -> DidMerge {
         let before_len = to.free.len();
         to.free.extend(from.free);
-        let did_change = before_len != to.free.len();
+        let free_merge = DidMerge(before_len != to.free.len(), true);
 
-        if !from.beta_extract.is_empty()
+        let beta_merge = if !from.beta_extract.is_empty()
             && (to.beta_extract.is_empty() || to.beta_extract.len() > from.beta_extract.len())
         {
             to.beta_extract = from.beta_extract;
-            return DidMerge(true, true);
-        }
-        DidMerge(did_change, true) // TODO: more precise second bool
+            DidMerge(true, true)
+        } else {
+            DidMerge(false, true) // TODO: more precise second bool
+        };
+
+        let nat_merge =
+            egg::merge_option(&mut to.simple_nat, from.simple_nat, |to_nat, from_nat| {
+                if to_nat.len() > from_nat.len() {
+                    *to_nat = from_nat;
+                    DidMerge(true, true)
+                } else {
+                    DidMerge(false, true)
+                }
+            });
+
+        free_merge | beta_merge | nat_merge
     }
 
     fn make(egraph: &mut EGraph<Rise, RiseAnalysis>, enode: &Rise) -> AnalysisData {
@@ -64,12 +94,33 @@ impl Analysis<Rise> for RiseAnalysis {
                 .copied()
                 .collect(),
         };
+
         let empty = enode.any(|id| egraph[id].data.beta_extract.is_empty());
-        let beta_extract = if empty {
-            RecExpr::default()
+        let (beta_extract, simple_nat) = if empty {
+            (RecExpr::default(), None)
         } else {
-            enode.join_recexprs(|id| egraph[id].data.beta_extract.as_ref())
+            let expr = enode.join_recexprs(|id| egraph[id].data.beta_extract.as_ref());
+            let simple_nat = try_simplify(&expr).ok();
+            (expr, simple_nat)
         };
-        AnalysisData { free, beta_extract }
+
+        AnalysisData {
+            free,
+            beta_extract,
+            simple_nat,
+        }
+    }
+
+    fn modify(egraph: &mut EGraph<Rise, RiseAnalysis>, id: Id) {
+        if let Some(expr) = egraph[id].data.simple_nat.clone() {
+            let added = egraph.add_expr(&expr);
+            egraph.union(id, added);
+
+            // to not prune, comment this out
+            // egraph[id].nodes.retain(egg::Language::is_leaf);
+
+            // #[cfg(debug_assertions)]
+            egraph[id].assert_unique_leaves();
+        }
     }
 }
